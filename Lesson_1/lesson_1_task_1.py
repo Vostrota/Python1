ce984ad238ad6aebda0fd33bfcd46e7f7b14ae3d
vostota <vostrota@gmail.com>
2024-05-16 16:20:43 +0300

    first hw

---
+++ Lesson_1/lesson_1_task_1.py
@@ -0,0 +1,2 @@
+my_name = "Vlada"
+print (my_name)
---
+++ Lesson_1/lesson_1_task_6.py
@@ -0,0 +1,33 @@
+def consol1 ():
+    print ("Вывести в консоль 1")
+def consol2 ():
+    print ("Вывести в консоль 2")
+def consol3 ():
+    print ("Вывести в консоль 3")
+def consol4 ():
+    print ("Вывести в консоль 4")
+def consol5 ():
+    print ("Вывести в консоль 5")
+def consol6 (): 
+    print ("Вывести в консоль 6")
+def consol7 ():
+    print ("Вывести в консоль 7")
+def consol8 ():
+    print ("Вывести в консоль 8")
+def consol9 ():
+    print ("Вывести в консоль 9")
+def consol0 ():
+    print ("Вывести в консоль 0")
+
+print("8", end="")
+print("8", end="")
+print("0", end="")
+print("0", end="")
+print("5", end="")
+print("5", end="")
+print("5", end="")
+print("3", end="")
+print("5", end="")
+print("3", end="")
+print("5", end="")
+
