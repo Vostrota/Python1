544744be850e82dfc8bb990454b919f8c8840b6c
vostota <vostrota@gmail.com>
2024-07-19 19:50:52 +0300

    исправления 1

--- Lesson_7/storePage.py
+++ Lesson_7/storePage.py
@@ -9,54 +9,43 @@ class Store:
     def __init__(self, browser):
         self.browser = browser
         self.browser.get("https://www.saucedemo.com/")
+        self.browser.implicitly_wait(3)
 
     def user_name(self, value):
         self.browser.find_element(By.ID, "user-name").send_keys(value)
-        sleep(3)
-
+    
     def password(self, value):
         self.browser.find_element(By.ID, "password").send_keys(value)
-        sleep(3)
-
+    
     def login_button(self):
         self.browser.find_element(By.ID, "login-button").click()
-        sleep(3)
-
+    
     def add_sauce_labs_backpack(self):
         self.browser.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
-        sleep(3)
-
+    
     def add_sauce_labs_bolt_t_shirt(self):
         self.browser.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
-        sleep(3)
-
+    
     def add_sauce_labs_onesie(self):
         self.browser.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
-        sleep(3)
-
+    
     def open_cart(self):
         self.browser.find_element(By.ID, "shopping_cart_container").click()
-        sleep(3)
-
+    
     def click_checkout(self):
         self.browser.find_element(By.ID, "checkout").click()
-        sleep(3)
-
+    
     def first_name(self, value):
         self.browser.find_element(By.ID, "first-name").send_keys(value)
-        sleep(3)
-
+    
     def last_name(self, value):
         self.browser.find_element(By.ID, "last-name").send_keys(value)
-        sleep(3)
-
+    
     def postal_code(self, value):
         self.browser.find_element(By.ID, "postal-code").send_keys(value)
-        sleep(3)
     
     def click_continue(self):
         self.browser.find_element(By.ID, "continue").click()
-        sleep(3)
     
     def total_result(self):
         txt = self.browser.find_element(By.XPATH, "//*[@id='checkout_summary_container']/div/div[2]/div[8]").text
--- Lesson_7/test_form.py
+++ Lesson_7/test_form.py
@@ -1,11 +1,31 @@
 import pytest
-from time import sleep
 from formPage import Form
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-def test_form():
+def test_red_element():
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    form = Form(browser)
+    form.first_name("Иван")
+    form.last_name("Петров")
+    form.adress("Ленина, 55-3")
+    form.email("test@skypro.com")
+    form.phone("+7985899998787")
+    form.zip_cod('""')
+    form.city("Москва")
+    form.country("Россия")
+    form.job_position("QA")
+    form.company("SkyPro")
+    form.send_form()
+
+    as_is = 1
+    to_be = form.empty_zip_result_is_red()
+    assert as_is == to_be
+
+    browser.quit()
+
+def test_green_elements():
     browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     form = Form(browser)
     form.first_name("Иван")
@@ -19,7 +39,12 @@ def test_form():
     form.job_position("QA")
     form.company("SkyPro")
     form.send_form()
-    form.zip_cod_is_red()
-    form.zip_cod_is_green()
+
+    classes = form.other_elements_result_is_green()
+    assert 'alert-success' in classes
+
+    browser.quit()
+
+
 
 
